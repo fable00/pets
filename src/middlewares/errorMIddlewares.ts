26a4d29ae39a5c2b742293433cf17172182c3828
simao00 <simaomcedo00@gmail.com>
2024-10-22 19:22:04 -0300

    22/10

--- src/middlewares/errorMIddlewares.ts
+++ src/middlewares/errorMIddlewares.ts
@@ -1,16 +1,21 @@
-import { ErrorRequestHandler, NextFunction, Request, Response } from "express";
+import { ErrorRequestHandler, Request, Response } from "express";
 import { ValidationError } from "sequelize";
 import { ZodError } from "zod";
+import { NotFoundError } from "../services/exceptions";
 
 export const errorHandler =(
   err: ErrorRequestHandler,
   req: Request,
   res: Response,
-  next: NextFunction,
-
 ) =>{
   console.error("Got an error", err)
   if (err instanceof ZodError) {
-    res.status(404)
+    res.status(404).json({message: "Invalid Data"})
+  }else if (err instanceof NotFoundError){
+    res.status(404).json({message: err.message})
+  }else if (err instanceof ValidationError){
+    res.status(422).json({message: err.message})
+  }else{
+    res.status(500).json({message: err})
   }
 }
